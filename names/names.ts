2d80bd86026b7437ca7b9a07337f9f1a54f3ded6
Alexander Sisco <alex@lexpierce.com>
2025-08-15 17:14:37 -0700

    Add a database

---
+++ names/names.ts
@@ -0,0 +1,13 @@
+import { api } from "encore.dev/api";
+
+const mydb = new SQLDatabase("encore_hello_worlds", {
+  migrations: "./migrations",
+});
+
+export const getUser = api(
+ { expose: true, method: "GET", path: "/names/:id" },
+ async ({id}: {id:number}): Promise<{ id: number; name: string }> => {
+   return await mydb.queryRow`SELECT * FROM users WHERE id = ${id}` as { id: number; name: string };
+ }
+);
+
